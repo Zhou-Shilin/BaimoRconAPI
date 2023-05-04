3ae9bd7b01da69fe588d99d5617cef8aa1ece399
BaimoQilin <baimoqilin@gmail.com>
2023-05-04 09:17:46 +0000

    dev0.2

--- src/main/java/top/baimoqilin/BaimoRconAPI/Main.java
+++ src/main/java/top/baimoqilin/BaimoRconAPI/Main.java
@@ -1,39 +1,91 @@
-package net.cyberflame.plugin;
+package top.baimoqilin.BaimoRconAPI;
 
-import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitTask;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 
-import net.cyberflame.plugin.commands.RootCommand;
-import net.cyberflame.plugin.listeners.PlayerJoinListener;
-import net.cyberflame.plugin.tasks.Task;
+public class Main extends JavaPlugin implements CommandExecutor {
 
-public class Main extends JavaPlugin {
-    
     @Override
-    public void onEnable () {
-        this.saveDefaultConfig();
-	    
-        this.getCommand("rootcommand").setExecutor(new RootCommand());
-        
-        Bukkit.getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
-
-        final long taskRepeatEvery = this.getConfig().getInt("task-repeat-every") * 20L;
-        BukkitTask task = new Task().runTaskLater(this, taskRepeatEvery);
-	    
-	// This is a regular Runnable example just in case I have a use for it,
-	// though I'm not making a Runnable example file
-	Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
-			public void run() {
-				//remove the block placed.
-				System.out.println("start message XD");
-			}
-		}, 20L * 2L * 3l);
+    public void onEnable() {
+        getCommand("baimoapi").setExecutor(this);
+        getLogger().info("BaimoRconAPI enabled");
     }
 
-    private static Main instance;
-
-    public static Main getInstance () {
-        return Main.instance;
+    @Override
+    public void onDisable() {
+        getLogger().info("BaimoRconAPI disabled");
     }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (label.equalsIgnoreCase("baimoapi")) {
+            if (args.length < 3) {
+                sender.sendMessage(ChatColor.RED + "Invalid command usage. Usage: /baimoapi player <getPos/getUuid/getWorld/getBlood/getHunger> <player-name>");
+                return false;
+            }
+            if (args[0].equalsIgnoreCase("player")) {
+                if (args[1].equalsIgnoreCase("getPos")) {
+                    Player target = Bukkit.getPlayer(args[2]);
+                    if (target != null) {
+                        Location loc = target.getLocation();
+                        sender.sendMessage("x=" + loc.getBlockX() + ", y=" + loc.getBlockY() + ", z=" + loc.getBlockZ());
+                        return true;
+                    } else {
+                        sender.sendMessage("ERROR=Player not found.");
+                        return false;
+                    }
+                } else if (args[1].equalsIgnoreCase("getUuid")) {
+                    Player target = Bukkit.getPlayer(args[2]);
+                    if (target != null) {
+                        sender.sendMessage("UUID=" + target.getUniqueId());
+                        return true;
+                    } else {
+                        sender.sendMessage("ERROR=Player not found.");
+                        return false;
+                    }
+                } else if (args[1].equalsIgnoreCase("getWorld")) {
+                    Player target = Bukkit.getPlayer(args[2]);
+                    if (target != null) {
+                        sender.sendMessage("World=" + target.getWorld().getName());
+                        return true;
+                    } else {
+                        sender.sendMessage("ERROR=Player not found.");
+                        return false;
+                    }
+                } else if (args[1].equalsIgnoreCase("getHealth")) {
+                    Player target = Bukkit.getPlayer(args[2]);
+                    if (target != null) {
+                        double healthValue = target.getHealth();
+                        sender.sendMessage("Blood=" + healthValue);
+                        return true;
+                    } else {
+                        sender.sendMessage("ERROR=Player not found.");
+                        return false;
+                    }
+                } else if (args[1].equalsIgnoreCase("getHunger")) {
+                    Player target = Bukkit.getPlayer(args[2]);
+                    if (target != null) {
+                        sender.sendMessage("Hunger=" + target.getFoodLevel());
+                        return true;
+                    } else {
+                        sender.sendMessage("ERROR=Player not found.");
+                        return false;
+                    }
+                } else {
+                    sender.sendMessage(ChatColor.RED + "ERROR=Invalid subcommand.");
+                    return false;
+                }
+            } else {
+                sender.sendMessage(ChatColor.RED + "ERROR=Invalid command.");
+                return false;
+            }
+        }
+        return false;
+     }
 }
