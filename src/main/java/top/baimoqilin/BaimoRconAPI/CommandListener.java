b353074e87a6eb2945be9135f43e2fc5b266fcfd
BaimoQilin <baimoqilin@gmail.com>
2023-05-11 12:12:13 +0000

    Fix CC

--- src/main/java/top/baimoqilin/BaimoRconAPI/CommandListener.java
+++ src/main/java/top/baimoqilin/BaimoRconAPI/CommandListener.java
@@ -1,6 +1,6 @@
 package top.baimoqilin.BaimoRconAPI;
 
-import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
